939fadb77539d4a90dafcb12d61a6a78eb8c630b
Martin Duong <tun82434@temple.edu>
2024-03-20 10:25:39 -0400

    added code

---
+++ receiptifyv1/config.js
@@ -0,0 +1,8 @@
+/**
+ * Open your spotify dashboard
+ * https://beta.developer.spotify.com/dashboard
+ */
+
+const client_id = '792207d6524f4255a1730e478d8b66f6';
+const redirect_uri = 'http://ajreisc.github.io';
+const client_secret = 'fd5c90696d984ca7a65a54853f340c70';
