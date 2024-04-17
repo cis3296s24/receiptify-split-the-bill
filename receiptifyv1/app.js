1fe9a6bc63711a11ac28597d7fa8f1bcb33f822a
Devlin Leonard <devlin.leonard@sap.com>
2024-04-16 22:14:10 -0400

    clean up db
    cleaned up db from hour long sessions, stopped duplication of session ids

--- receiptifyv1/app.js
+++ receiptifyv1/app.js
@@ -80,6 +80,25 @@ var generateSessionID = function () {
   for (var i = 0; i < 6; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
+  const fileStream = fs.createReadStream("users.csv");
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+  rl.on('line', (line) => {
+    const row = line.split(',');
+    if (row[2] == text) {
+      text = generateSessionID();
+    }
+  }); 
+  /*
+  for await (const line of rl) {
+    const row = line.split(',');
+    if (row[2] == text) {
+      text = generateSessionID();
+    }
+  }
+  */
   return text;
 };
 
@@ -210,12 +229,29 @@ async function processFile(filePath, sessionID) {
       crlfDelay: Infinity,
     });
     const users = [];
+    var newDB = "";
+    var first = true;
     for await (const line of rl) {
+      if(first){
+        newDB += line + "\n";
+        first = false;
+        continue;
+      }
       const row = line.split(',');
       if (row[2] == sessionID) {
         users.push(row[0]);
       }
+      if((row[3] - 1 + 3600000 > Date.now())){
+        newDB += line + "\n";
+      };
     }
+    fs.writeFile("users.csv", newDB, err => {
+      if (err) {
+        console.log(err.message);
+    
+        throw err;
+      }
+  });
     await rl.close();
 
     return users;
@@ -281,10 +317,8 @@ app.get('/callback', function (req, res) {
         );
         
         // Gets time (year-month-day hour:min:sec)
-        var currentDate = new Date();
-        var access_time = currentDate.getFullYear() + '-' + (currentDate.getMonth()+1) + '-' + currentDate.getDate() + ' ' + 
-          currentDate.getHours() + ':' + currentDate.getMinutes() + ':' + currentDate.getSeconds();
-        console.log("Token Access Time: ", access_time);
+        console.log(Date.now());
+        var access_time = Date.now();
         
         // Writing to users.csv (Database)
         fs.appendFile('users.csv', ('\n'+ profile.display_name + ',' + access_token +',' + sessionID + ','  + access_time + ','), (err) => {
